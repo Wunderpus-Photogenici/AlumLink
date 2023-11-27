6db415732f34e897e3e9a3be8655854cd131fbbb
Ezekiel Mohr <85984467+Ezmr7@users.noreply.github.com>
2023-11-27 09:30:12 -0600

    Delete src/interviewRouter.js

--- src/interviewRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const studentController = require('./controllers/studentController');
-const router = express.Router();
-
-//interview router
-router.get('/interview', controller.getPosts(), (req, res) => {
-    res.send()
-})
