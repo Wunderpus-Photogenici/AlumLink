93a9b24914dce14a3c9e13bb8dca79fc6691fc62
Ezekiel Mohr <85984467+Ezmr7@users.noreply.github.com>
2023-11-27 09:47:49 -0600

    Front/Back Merge

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './components/App';
-import "./components/styles.css"
+import "./styles.css"
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
